3181fa26cccd4150e92b4c108284b062953552fb
Abhishek Singh <abhisheksinghxix@gmail.com>
2023-02-25 03:35:18 +0530

    Add CustomAPIErrors

---
+++ server/src/errors/UnauthorizedError.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require('./CustomAPIError');
+const { StatusCodes } = require('http-status-codes');
+
+class UnauthorizedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.code = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+module.exports = UnauthorizedError;
---
+++ server/src/errors/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BadRequestError: require('./BadRequestError'),
+  NotFoundError: require('./NotFoundError'),
+  UnauthorizedError: require('./UnauthorizedError'),
+};
