86d026e9a8d1f667d74f5ca1520fc4e0d9e1fa00
Abhishek Singh <abhisheksinghxix@gmail.com>
2023-02-26 17:43:30 +0530

    Add authentication middleware

---
+++ server/src/middleware/authentication.js
@@ -0,0 +1,24 @@
+const { UnauthorizedError } = require('../errors');
+const jwt = require('jsonwebtoken');
+const { createTokenUser } = require('../utils');
+
+const authentication = async (req, res, next) => {
+  const { authToken } = req.signedCookies;
+
+  if (!authToken)
+    throw new UnauthorizedError(
+      'you need to be logged in to perform this action.',
+    );
+
+  let tokenUser = null;
+
+  try {
+    tokenUser = createTokenUser(jwt.verify(authToken, process.env.JWT_SECRET));
+  } catch (error) {
+    throw new UnauthorizedError('invalid token.');
+  }
+
+  res.locals.user = tokenUser;
+
+  next();
+};
