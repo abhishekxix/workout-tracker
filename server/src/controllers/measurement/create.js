81d4449164ee17ae26fd5fe0fdc056c838c80d9c
Abhishek Singh <abhisheksinghxix@gmail.com>
2023-03-01 11:03:48 +0530

    untracked files on api/measurement: 7ec5f9a Add deleteDailyStat route

---
+++ server/src/controllers/measurement/create.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require('http-status-codes');
+const { Measurement } = require('../../models');
+
+const create = async (req, res) => {
+  const { dailyStatID, bodyPart, measurement } = req.body;
+  const { user } = res.locals;
+
+  const createdMeasurement = await Measurement.create({
+    dailyStatID,
+    bodyPart,
+    measurement,
+    userID: user.userID,
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'success', measurement: createdMeasurement });
+};
+
+module.exports = create;
